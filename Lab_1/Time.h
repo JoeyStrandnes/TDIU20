4f2943cc9182d4dce34ec117b44800f51980eb19
josst471 <joey.makdesi1@hotmail.com>
2020-01-23 13:28:46 +0100

    Init

---
+++ Lab_1/Time.h
@@ -0,0 +1,21 @@
+// Denna fil ska innehålla deklarationer för de typer och funktioner
+// som behövs
+#ifndef TIME_H
+#define TIME_H
+
+class Time {
+
+public:
+  Time(int Hour, int Minute, int Second);
+
+
+private:
+
+  int Hour;
+  int Minute;
+  int Second;
+
+
+}
+
+#endif
