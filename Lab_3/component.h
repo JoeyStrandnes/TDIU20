0bf2fdb23cc48642ec69c65b83d72e0c585488ac
josst471 <joey.makdesi1@hotmail.com>
2020-02-20 14:55:42 +0100

    Skeleton

---
+++ Lab_3/component.h
@@ -0,0 +1,39 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+class COMPONENT{
+
+public:
+
+
+
+private:
+
+  class battery: public COMPONENT {
+  public:
+
+  private:
+
+  };
+  /*
+  class resistor: public COMPONENT {
+  public:
+
+  private:
+
+  };
+
+  class capacitor: public COMPONENT {
+  public:
+
+  private:
+
+  }
+  */
+
+
+}
+
+
+
+#endif
